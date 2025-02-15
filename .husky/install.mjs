0185eebd6857e789c7fd3fae650838823fe79e7e
ryanbas21 <ryanbas21@gmail.com>
2025-02-14 20:49:23 -0700

    chore: fix-logic-add-tests

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -2,5 +2,10 @@
 if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
   process.exit(0);
 }
+<<<<<<< HEAD
 const husky = (await import('husky')).default;
 husky();
+=======
+const husky = (await import('husky')).default
+console.log(husky())
+>>>>>>> aeefdd88 (chore: fix-logic-add-tests)
