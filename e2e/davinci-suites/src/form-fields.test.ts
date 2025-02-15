a1e5cb9ce96e1c7996847bb51e7301eab2f8a141
ryanbas21 <ryanbas21@gmail.com>
2025-02-14 20:49:23 -0700

    chore: fix-logic-add-tests

--- e2e/davinci-suites/src/form-fields.test.ts
+++ e2e/davinci-suites/src/form-fields.test.ts
@@ -1,18 +1,44 @@
 import { expect, test } from '@playwright/test';
-import { asyncEvents } from './utils/async-events.js';
 
-test('Test happy paths on test page', async ({ page }) => {
-  const { navigate } = asyncEvents(page);
-  await navigate('/?clientId=60de77d5-dd2c-41ef-8c40-f8bb2381a359');
+test('Should render form fields', async ({ page }) => {
+  await page.goto('http://localhost:5829/?clientId=60de77d5-dd2c-41ef-8c40-f8bb2381a359');
+  await page.getByRole('heading', { name: 'Select Test Form' }).click();
+  await page.getByRole('button', { name: 'Form Fields' }).click();
+  await page.getByRole('textbox', { name: 'Text Input Label' }).click();
+  await page.getByRole('textbox', { name: 'Text Input Label' }).click();
 
-  console.log(page.url());
-  expect(page.url()).toBe('http://localhost:5829/?clientId=60de77d5-dd2c-41ef-8c40-f8bb2381a359');
+  const txtInput = page.getByRole('textbox', { name: 'Text Input Label' });
+  await txtInput.fill('This is some text');
+  expect(txtInput).toHaveValue('This is some text');
 
-  await expect(page.getByText('Select Test Form')).toBeVisible();
+  const flowLink = page.getByRole('button', { name: 'Flow Link' });
+  await flowLink.click();
 
-  const formFields = page.getByRole('button', { name: 'Form Fields' });
-  const formValidation = page.getByRole('button', { name: 'Form Validation' });
+  const flowButton = page.getByRole('button', { name: 'Flow Button' });
+  await flowButton.click();
 
-  await expect(formFields).toBeVisible();
-  await expect(formValidation).toBeVisible();
+  await page.getByRole('button', { name: 'Submit' }).click();
+});
+
+test('should render form validation fields', async ({ page }) => {
+  await page.goto('http://localhost:5829/?clientId=60de77d5-dd2c-41ef-8c40-f8bb2381a359');
+  await expect(page.getByRole('link', { name: 'Vite logo' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Form Validation' })).toBeVisible();
+  await expect(page.locator('#form')).toContainText('Form Validation');
+  await page.getByRole('button', { name: 'Form Validation' }).click();
+  await expect(page.getByRole('heading', { name: 'Form Fields Validation' })).toBeVisible();
+  await expect(page.getByText('Username')).toBeVisible();
+  await expect(page.getByRole('textbox', { name: 'Username' })).toBeVisible();
+  await expect(page.getByText('Email Address')).toBeVisible();
+  await expect(page.getByRole('textbox', { name: 'Email Address' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Submit' })).toBeVisible();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('sdk-user');
+  await expect(page.getByRole('textbox', { name: 'Username' })).toHaveValue('sdk-user');
+  await page.getByRole('textbox', { name: 'Email Address' }).click();
+  await page.getByRole('textbox', { name: 'Email Address' }).fill('sdk-user@user.com');
+  await expect(page.getByRole('textbox', { name: 'Email Address' })).toHaveValue(
+    'sdk-user@user.com',
+  );
+  await page.getByRole('button', { name: 'Submit' }).click();
 });
