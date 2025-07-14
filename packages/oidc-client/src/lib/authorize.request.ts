0b1e1ddcc684eec949f0830732dda4c9ba429f24
Justin Lowery <justin@cerebrl.com>
2025-07-14 13:36:19 -0500

    feat(oidc-client): introduce Effect in authorize

--- packages/oidc-client/src/lib/authorize.request.ts
+++ packages/oidc-client/src/lib/authorize.request.ts
@@ -1,5 +1,6 @@
 import { iFrameManager } from '@forgerock/iframe-manager';
 import { createAuthorizeUrl, GetAuthorizationUrlOptions } from '@forgerock/sdk-oidc';
+import { Micro } from 'effect';
 
 import { createAuthorizeOptions, handleError, handleResponse } from './authorize.request.utils.js';
 
@@ -28,32 +29,51 @@ export async function authorize(
        * We need to make a post (or a get) request and both are supported by
        * PingOne.
        */
-      const authorizeUrl = await createAuthorizeUrl(authorizePath, {
-        ...optionsWithDefaults,
-        prompt: 'none',
-        responseMode: 'pi.flow',
-      });
-      const res = await fetch(authorizeUrl, {
-        method: 'POST',
-        credentials: 'include',
-      });
+      const authorizeUrlMicro = Micro.promise(() =>
+        createAuthorizeUrl(authorizePath, {
+          ...optionsWithDefaults,
+          prompt: 'none',
+          responseMode: 'pi.flow',
+        }),
+      );
 
-      response = await res.json();
+      const fetchMicro = (url: string) =>
+        Micro.promise(() =>
+          fetch(url, {
+            method: 'POST',
+            credentials: 'include',
+          }),
+        );
+
+      const authorizeRequest = authorizeUrlMicro.pipe(
+        Micro.flatMap(fetchMicro),
+        Micro.flatMap((response) => Micro.promise(response.json)),
+      );
+      response = await Micro.runPromise(authorizeRequest);
     } else {
-      const authorizeUrl = await createAuthorizeUrl(authorizePath, {
-        ...optionsWithDefaults,
-        prompt: 'none',
-      });
-      response = await iFrameManager().getParamsByRedirect({
-        url: authorizeUrl,
-        /***
-         * https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.2
-         * The client MUST ignore unrecognized response parameters.
-         */
-        successParams: ['code', 'state'],
-        errorParams: ['error', 'error_description'],
-        timeout: config.serverConfig.timeout || 3000,
-      });
+      const authorizeUrlMicro = Micro.promise(() =>
+        createAuthorizeUrl(authorizePath, {
+          ...optionsWithDefaults,
+          prompt: 'none',
+        }),
+      );
+
+      const iframeMicro = (url: string) =>
+        Micro.promise(() =>
+          iFrameManager().getParamsByRedirect({
+            url,
+            /***
+             * https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.2
+             * The client MUST ignore unrecognized response parameters.
+             */
+            successParams: ['code', 'state'],
+            errorParams: ['error', 'error_description'],
+            timeout: config.serverConfig.timeout || 3000,
+          }),
+        );
+
+      const authorizeRequest = authorizeUrlMicro.pipe(Micro.flatMap(iframeMicro));
+      response = await Micro.runPromise(authorizeRequest);
     }
 
     // Normalize response, for both success and failure, to handle both
