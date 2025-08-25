34635957a6f9efb753c1d0d87f3b748bc4440fd2
Ryan Bas <ryanbas21@gmail.com>
2025-08-25 16:21:49 -0600

    chore: update-typechecking

--- e2e/mock-api-v2/src/handlers/authorize.handler.ts
+++ e2e/mock-api-v2/src/handlers/authorize.handler.ts
@@ -4,9 +4,9 @@
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
-import { Effect } from 'effect';
+import { Effect, pipe } from 'effect';
 import { MockApi } from '../spec.js';
-import { HttpApiBuilder, HttpServerResponse } from '@effect/platform';
+import { HttpApiBuilder, HttpApiError, HttpServerResponse } from '@effect/platform';
 import { getFirstElementAndRespond } from '../services/mock-env-helpers/index.js';
 
 const AuthorizeHandlerMock = HttpApiBuilder.group(MockApi, 'Authorization', (handlers) =>
@@ -14,9 +14,15 @@ const AuthorizeHandlerMock = HttpApiBuilder.group(MockApi, 'Authorization', (han
     Effect.gen(function* () {
       const acr_value = urlParams?.acr_values ?? '';
 
-      const response = yield* getFirstElementAndRespond(urlParams);
+      const body = yield* getFirstElementAndRespond(urlParams);
 
-      return HttpServerResponse.setCookie(response, 'acr_values', acr_value);
+      const res = yield* pipe(
+        HttpServerResponse.json(body),
+        HttpServerResponse.setCookie('acr_values', acr_value),
+        Effect.catchTag('CookieError', () => new HttpApiError.InternalServerError()),
+      );
+
+      return res;
     }).pipe(Effect.withSpan('DavinciAuthorize')),
   ),
 );
