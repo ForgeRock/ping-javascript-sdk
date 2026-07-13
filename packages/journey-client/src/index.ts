69eec27436007bf22aa6b3b6a0819ac9e1b15477
Ryan Bas <ryanbas21@gmail.com>
2026-07-13 13:27:28 -0600

    feat(journey-client): add-create-journey-step-to-api
    Add createJourneyStep to the journey-client api

--- packages/journey-client/src/index.ts
+++ packages/journey-client/src/index.ts
@@ -8,6 +8,9 @@
 export * from './lib/client.store.js';
 export * from './types.js';
 
+// required to help deserialize a step after it's been serialized
+export { createJourneyStep } from './lib/step.utils.js';
+
 // Re-export helpers and constants from internal packages that consumers need
 export { callbackType } from '@forgerock/sdk-types';
 export { makeJourneyConfig } from '@forgerock/sdk-utilities';
