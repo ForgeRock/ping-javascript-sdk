a9ac960354759598339ede3b09f8c9ac61339915
Ryan Bas <ryanbas21@gmail.com>
2026-07-13 13:27:28 -0600

    feat(journey-client): add-create-journey-step-to-api
    Add createJourneyStep to the journey-client api

--- packages/journey-client/src/index.ts
+++ packages/journey-client/src/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 - 2025 Ping Identity Corporation. All rights reserved.
+ * Copyright (c) 2020 - 2026 Ping Identity Corporation. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -7,6 +7,9 @@
 
 export * from './lib/client.store.js';
 export * from './types.js';
+
+// required to help deserialize a step after it's been serialized
+export { createJourneyStep } from './lib/step.utils.js';
 
 // Re-export types from internal packages that consumers need
 export { callbackType } from '@forgerock/sdk-types';
