ea83f9193b848dbe955a61304e52d73c62c43f18
Eugenio Bettini <eugenio.bettini+gh@gmail.com>
2026-07-23 21:14:52 +0200

    refactor(recognize): separate runtime and type exports into index.ts and types.ts

--- packages/recognize/src/index.ts
+++ packages/recognize/src/index.ts
@@ -1,17 +1,3 @@
 export { recognize } from './lib/recognize.js';
 export { createInternalError } from './lib/functions/recognize-error.js';
 export { RecognizeErrorCode } from './lib/defs/recognize-error-code.js';
-export type { RecognizeError } from './lib/functions/recognize-error.js';
-export type {
-  RecognizeSessionType,
-  RecognizeWebComponent,
-  RecognizeWebComponentClient,
-  RecognizeWebComponentCompleteData,
-  RecognizeWebComponentConfiguration,
-  RecognizeWebComponentEvent,
-  RecognizeWebComponentInitOptions,
-  RecognizeWebComponentObserver,
-  RecognizeWebComponentStepChangeEventDetail,
-  RecognizeWebComponentUnsubscribe,
-  RecognizeWebComponentVideoFrameQualityEventDetail,
-} from './lib/recognize.types.js';
---
+++ packages/recognize/src/types.ts
@@ -0,0 +1,15 @@
+export type { RecognizeError } from './lib/functions/recognize-error.js';
+export type { RecognizeErrorCode } from './lib/defs/recognize-error-code.js';
+export type {
+  RecognizeSessionType,
+  RecognizeWebComponent,
+  RecognizeWebComponentClient,
+  RecognizeWebComponentCompleteData,
+  RecognizeWebComponentConfiguration,
+  RecognizeWebComponentEvent,
+  RecognizeWebComponentInitOptions,
+  RecognizeWebComponentObserver,
+  RecognizeWebComponentStepChangeEventDetail,
+  RecognizeWebComponentUnsubscribe,
+  RecognizeWebComponentVideoFrameQualityEventDetail,
+} from './lib/recognize.types.js';
