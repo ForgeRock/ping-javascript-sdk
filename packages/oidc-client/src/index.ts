1c037734aa19f0eba4ca6611bcaa623f399b3f3c
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2026-07-30 16:45:33 -0400

    chore: add explicit client exports in default entry points

--- packages/oidc-client/src/index.ts
+++ packages/oidc-client/src/index.ts
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2025 Ping Identity Corporation. All rights reserved.
+ * Copyright (c) 2025 - 2026 Ping Identity Corporation. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
-// oidc and createClientStore are re-exported via types.js
+export { oidc } from './lib/client.store.js';
 export * from './types.js';
