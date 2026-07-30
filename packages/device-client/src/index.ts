294f32a2dc2ef533e670c9249349d7cee7985429
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2026-07-30 16:45:33 -0400

    chore: add explicit client exports in default entry points

--- packages/device-client/src/index.ts
+++ packages/device-client/src/index.ts
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2025 Ping Identity Corporation. All rights reserved.
+ * Copyright (c) 2025 - 2026 Ping Identity Corporation. All rights reserved.
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
-// deviceClient is re-exported via types.js
+export { deviceClient } from './lib/device.store.js';
 export * from './types.js';
