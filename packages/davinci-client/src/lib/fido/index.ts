b6117d1ea4312954980ef73d3ab74cf7a51c37b6
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2026-07-30 16:37:44 -0400

    chore(davinci-client): add fido entry point

---
+++ packages/davinci-client/src/lib/fido/index.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2026 Ping Identity Corporation. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+// Re-export FIDO module and types for package.json `./fido` entry point
+export { fido } from './fido.js';
+export * from './fido.types.js';
