837f9745bd074f188eabb85ca821281b4e25a8b0
Vatsal Parikh <vatsalparikh11@outlook.com>
2026-09-08 10:18:23 -0700

    chore: fix copyright headers per review feedback

--- e2e/oidc-suites/playwright.config.ts
+++ e2e/oidc-suites/playwright.config.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025 - 2026 Ping Identity Corporation. All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
 import { workspaceRoot } from '@nx/devkit';
 import { defineConfig } from '@playwright/test';
 
